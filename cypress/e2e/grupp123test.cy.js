ef944823145218b6776a7f9b3fb4f8efac6d5798
MurtadaAmeri <amerimurtada@gmail.com>
2023-11-10 14:57:27 +0100

    klar

--- cypress/e2e/grupp123test.cy.js
+++ cypress/e2e/grupp123test.cy.js
@@ -1,5 +1,11 @@
-describe("template spec", () => {
+describe("show Aftonbladet button", () => {
   it("passes", () => {
-    cy.visit("http://localhost:5173");
+    cy.visit("http://localhost:5174");
+  });
+    it("should Show Button#Aftonbladet Button", () => {
+    cy.visit("http://localhost:5174");
+    cy.get("button#Aftonbladet").should("exist");
   });
 });
+
+
