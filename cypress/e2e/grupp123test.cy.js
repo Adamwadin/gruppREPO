eab24d7b59313541657d2f85137afe9dcad950d6
Marcus Gustavsson <mackan1892@outlook.com>
2023-11-10 14:57:46 +0100

    Marcus knapp

--- cypress/e2e/grupp123test.cy.js
+++ cypress/e2e/grupp123test.cy.js
@@ -1,5 +1,11 @@
-describe("template spec", () => {
+describe("show Marcus Button", () => {
   it("passes", () => {
     cy.visit("http://localhost:5173");
   });
+
+  it("should show marcus button#marcusButton", () => {
+    cy.visit("http://localhost:5173");
+    cy.get("button#marcusButton").should("exist");
+  });
+
 });
