5179704e9b6e9d3a49a56268bab62e6ad90c0f28
johan.hammarstedt@j-s-h.com <johan.hammarstedt@j-s-h.com>
2023-11-10 14:56:43 +0100

    bringthechaos

--- cypress/e2e/grupp123test.cy.js
+++ cypress/e2e/grupp123test.cy.js
@@ -1,5 +1,21 @@
-describe("template spec", () => {
-  it("passes", () => {
+describe("myButtonFunctionality", () => {
+  beforeEach(() => {
     cy.visit("http://localhost:5173");
+  });
+  it("passes", () => {
+    // cy.visit("http://localhost:5173");
+  });
+
+  it("should show a button", () => {
+    // cy.visit("http://localhost:5173");
+    cy.get("button").should("exist");
+  });
+
+  it("should have text on it", () => {
+    cy.get("button").should("contain.text", "knappjäveln");
+  });
+
+  it("should create a P-tag", () => {
+    cy.get("button").click();
   });
 });
